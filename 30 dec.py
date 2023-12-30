b4981460b7a03fd0009db98335bcac5535e0be4b
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2023-12-30 20:17:10 +0530

    Add files via upload

---
+++ 30 dec.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#oops
+class addition:
+    def __init__(self):
+        self.a=10
+        self.b=20
+    def res(self):
+        print(self.a+self.b)
+
+
+# In[2]:
+
+
+add_obj=addition()
+add_obj.res()
+
+
+# In[4]:
+
+
+class subscription:
+    def __init__(self):
+        self.a=30
+        self.b=10
+    def res(self):
+        print(self.a-self.b)
+
+
+# In[5]:
+
+
+add_obj=subscription()
+add_obj.res()
+
+
+# In[6]:
+
+
+class multiplication:
+    def __init__(self):
+        self.a=50
+        self.b=20
+    def res(self):
+        print(self.a*self.b)
+
+
+# In[7]:
+
+
+add_obj=multiplication()
+add_obj.res()
+
+
+# In[8]:
+
+
+class division:
+    def __init__(self):
+        self.a=80
+        self.b=10
+    def res(self):
+        print(self.a/self.b)
+
+
+# In[9]:
+
+
+add_obj=division()
+add_obj.res()
+
+
+# In[10]:
+
+
+#area of circle
+class area:
+    def __init__(self):
+        self.r=9
+    def res(self):
+        print(self.r*3.14)
+
+
+# In[11]:
+
+
+add_ob=area()
+add_obj.res()
+
+
+# In[26]:
+
+
+#area of rectangle
+class area:
+    def __init__(self):
+        self.l=9
+        self.w=9
+    def res(self):
+        print(self.l*self.w)
+
+
+# In[27]:
+
+
+add_obj=area()
+add_obj.res()
+
+
+# In[28]:
+
+
+#area of square
+class area:
+    def __init__(self):
+        self.a=7
+    def res(self):
+        print(self.a*self.a)
+
+
+# In[29]:
+
+
+add_obj=area()
+add_obj.res()
+
+
+# In[30]:
+
+
+#area of triangle
+class area:
+    def __init__(self):
+        self.b=9
+        self.h=8
+    def res(self):
+        print(self.b*self.h)
+
+
+# In[31]:
+
+
+add_obj=area()
+add_obj.res()
+
+
+# In[32]:
+
+
+#area of cube
+class area:
+    def __init__(self):
+        self.x=6
+    def res(self):
+        print(self.x*self.x*self.x)
+
+
+# In[33]:
+
+
+add_ob=area()
+add_ob.res()
+
+
+# In[ ]:
+
+
+
+
